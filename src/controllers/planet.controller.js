643135abdfdc4a32112966d10a12b644984cb683
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2019-07-21 19:18:31 -0300

    improvements on controller

--- src/controllers/planet.controller.js
+++ src/controllers/planet.controller.js
@@ -1,6 +1,10 @@
 const PlanetModel = require('../models/planet.model')
 const axios = require('axios')
 
+/**
+ * @param req
+ * @param res
+ */
 exports.getPlanet = (req, res) => {
     let query = {}
     if (req.query.name) {
@@ -8,21 +12,36 @@ exports.getPlanet = (req, res) => {
     }
     PlanetModel.find(query, (err, results) => {
         if (err) {
-            return res.send('Erro ao listar os planetas')
+            return res.send('Erro ao listar os planetas!')
+        }
+        if (results.length === 0) {
+            return res.send('Não existe nenhum planeta cadastrado!')
         }
         res.send(results)
     })
 }
 
+/**
+ * @param req
+ * @param res
+ */
 exports.getPlanetById = (req, res) => {
     PlanetModel.findById(req.params.id, (err, results) => {
         if (err) {
             return res.send('Erro! Esse planeta não existe')
         }
+        if (!results) {
+            return res.send('Esse planeta não existe!')
+        }
         res.send(results)
     })
 }
 
+/**
+ * @param req
+ * @param res
+ * @returns {*|void}
+ */
 exports.createPlanet = (req, res) => {
     if (!req.body.name) {
         return res.send('Informe o nome do planeta!')
@@ -36,17 +55,22 @@ exports.createPlanet = (req, res) => {
             return res.send('Não existe planeta com esse nome!')
         }
         if (resp.data.results[0].name !== req.body.name) {
-            return res.send('Existe um planeta com o nome próximo! Verifique o nome!')
+            return res.send('Existe um planeta com o nome próximo. Verifique o nome!')
         }
-        let arrayUrl = resp.data.results[0].url.split('/')
-        let lengthArrayUrl = arrayUrl.length
-        let positionId = lengthArrayUrl - 2
-        let planetId = arrayUrl[positionId]
+        if (!resp.data.results[0].url) {
+            return res.send('Não foi possível encontrar o código desse planeta!')
+        }
+        let url = resp.data.results[0].url
+        let planetId = getPlanetId(url)
 
         savePlanet(resp, req, res, planetId)
     }).catch(console.log('Erro'))
 }
 
+/**
+ * @param req
+ * @param res
+ */
 exports.deletePlanet = (req, res) => {
     PlanetModel.findByIdAndRemove(req.params.id, (err) => {
         if (err) {
@@ -56,6 +80,22 @@ exports.deletePlanet = (req, res) => {
     })
 }
 
+/**
+ * @param url
+ * @returns {string}
+ */
+function getPlanetId(url) {
+    let arrayUrl = url.split('/')
+    let positionId = arrayUrl.length - 2
+    return arrayUrl[positionId]
+}
+
+/**
+ * @param resp
+ * @param req
+ * @param res
+ * @param planetId
+ */
 function savePlanet(resp, req, res, planetId) {
     let planet = new PlanetModel(
         {
